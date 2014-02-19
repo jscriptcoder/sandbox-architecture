f18a3b49b13202ad76a12f1997ff8de0e54ebbc5
jscriptcoder <jscriptcoder@gmail.com>
2014-02-19 14:15:24 +0100

    added Sandbox.run unit test

--- test/specs/sb-spec.js
+++ test/specs/sb-spec.js
@@ -1,31 +1,90 @@
 describe('Global Sandbox API', function () {
 
-    it('registers modules [Sandbox.register]', function () {
+    beforeEach(function () {
 
-        var modules = Sandbox.__modules__;
-
-        Sandbox.register('Module1', function () {
+        Sandbox.register('ModuleTest1', function () {
             return {
                 name: 'module1',
                 func: function () { return 'attribute'; }
             };
         });
 
-        Sandbox.register('Module2', ['Module1'], function (toolbox) {
-            var mod1 = toolbox.Module1;
+        Sandbox.register('ModuleTest2', function (toolbox) {
             return {
-                name: 'child of ' + mod1.name,
+                name: 'module2',
+                num: 6
+            };
+        });
+
+        Sandbox.register('ModuleTest3', ['ModuleTest1', 'ModuleTest2'], function (toolbox) {
+            var mod1 = toolbox.ModuleTest1,
+                mod2 = toolbox.ModuleTest2;
+
+            return {
+                name: 'module3',
+                info: 'depends on ' + [mod1.name, mod2.name].join (' and '),
+                attr: mod1.func(),
+                num: mod2.num
+            };
+        });
+
+    });
+
+    afterEach(function () {
+        delete Sandbox.__modules__['ModuleTest1'];
+        delete Sandbox.__modules__['ModuleTest2'];
+        delete Sandbox.__modules__['ModuleTest3'];
+    });
+
+    it('registers modules [Sandbox.register]', function () {
+
+        var modules = Sandbox.__modules__;
+
+        expect(modules['ModuleTest1']).toBeDefined();
+        expect(modules['ModuleTest2']).toBeDefined();
+        expect(modules['ModuleTest3'].requires[0]).toEqual('ModuleTest1');
+        expect(modules['ModuleTest3'].requires[1]).toEqual('ModuleTest2');
+
+    });
+
+    it('runs an anonymous module [Sandbox.run]', function () {
+
+        var anonymModule = Sandbox.run(function () {
+            return {
+                name: 'anonynous',
+                value: true
+            };
+        });
+
+        expect(anonymModule).toBeDefined();
+        expect(anonymModule.name).toEqual('anonynous');
+        expect(anonymModule.value).toBeTruthy();
+
+        var anotherModule = Sandbox.run('ModuleTest1', function (toolbox) {
+            var mod1 = toolbox.ModuleTest1;
+            return {
+                info: 'depends on ' + mod1.name,
                 attr: mod1.func()
             };
         });
 
-        expect(modules['Module1']).toBeDefined();
-        expect(modules['Module2']).toBeDefined();
-        expect(modules['Module2'].requires[0]).toEqual('Module1');
+        expect(anotherModule).toBeDefined();
+        expect(anotherModule.info).toEqual('depends on module1');
+        expect(anotherModule.attr).toEqual('attribute');
+
+        var lastModule = Sandbox.run('ModuleTest3', function (toolbox) {
+            var mod3 = toolbox.ModuleTest3;
+            return {
+                info: mod3.info,
+                num: mod3.num
+            };
+        });
+
+        expect(lastModule).toBeDefined();
+        expect(lastModule.info).toEqual('depends on module1 and module2');
+        expect(lastModule.num).toEqual(6);
 
     });
-
-    it('runs an anonymous module [Sandbox.run]', function () {});
 
     it('starts modules [Sandbox.start]', function () {});
 
