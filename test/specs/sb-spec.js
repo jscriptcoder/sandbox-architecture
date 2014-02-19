9717f7d37ac5c38403ce0bd1b61038c835dc15ee
jscriptcoder <jscriptcoder@gmail.com>
2014-02-19 11:23:53 +0100

    unit tests started

---
+++ test/specs/sb-spec.js
@@ -0,0 +1,21 @@
+describe('Global Sandbox API', function () {
+
+    it('registers modules [Sandbox.register]', function () {});
+
+    it('runs an anonymous module [Sandbox.run]', function () {});
+
+    it('starts modules [Sandbox.start]', function () {});
+
+    it('registers and starts a module [Sandbox.use]', function () {});
+
+    it('starts all the modules at once [Sandbox.startAll]', function () {});
+
+    it('stops modules [Sandbox.stop]', function () {});
+
+    it('stops all the modules at once [Sandbox.stopAll]', function () {});
+
+    it('stops and removes a module [Sandbox.remove]', function () {});
+
+    it('removes all the modules at once [Sandbox.removeAll]', function () {});
+
+});
